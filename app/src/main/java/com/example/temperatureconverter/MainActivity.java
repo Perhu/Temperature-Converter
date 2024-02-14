b1a34a0497166a369ea6323fbe9ce072654dbcb0
Peter Rachev <peshodimrachev@gmail.com>
2024-02-14 04:24:18 +0200

    Separating the functionallity into a controller class

--- app/src/main/java/com/example/temperatureconverter/MainActivity.java
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.java
@@ -4,63 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.temperatureconverter.controller.ButtonController;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-//  buffer variable that is used to monitor if Celsius is changed
-    private double theCl=0;
-
+    //creating a controller
+    private ButtonController btnControl;
+    //Getting the View
+    public View getView(){
+        return findViewById(android.R.id.content);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Linking the controller
+        this.btnControl=new ButtonController(this);
     }
-//  when opening the application after being minimilzed
+//  when opening the application after being minimized
     @Override
     protected void onResume() {
         super.onResume();
         //reset the values
-        buttonResetAction(findViewById(android.R.id.content));
+        btnControl.buttonResetAction();
     }
-//    Getter for the Celsius field
-    private TextInputEditText getFieldCel()
-    {
-        return (TextInputEditText) this.findViewById(R.id.NumberBoxCel);
-    }
-//    getter for fahrenheit field
-    private TextInputEditText getFieldFh()
-    {
-        return (TextInputEditText) this.findViewById(R.id.NumberBoxFh);
-    }
-//    function to reset the values
-    public void buttonResetAction(View view) {
-        getFieldCel().setText("0");
-        getFieldFh().setText("32");
-    }
-//function to calculate and set the values
+    //Calling the controller functions from the Main
     public void buttonCalculateAction(View view) {
-        //parsing the values from String to double
-        double cl=Double.parseDouble(String.valueOf(getFieldCel().getText()));
-        double fh=Double.parseDouble(String.valueOf(getFieldFh().getText()));;
-        /*
-        the way i'm checking which field is changed is by comparing
-        the buffer variable to the current value of Celsius.
-        -in case Celsius is changed, Fahrenheit is calculated
-        -in case Celsius is not changed, that could mean the FH was changed,
-        therefore Celsius is calculated
-        -in case neither is changed, there would be no change(except maybe making CL from int to double)
-        -in case both are changed Celsius takes priority (it was not specified which one takes priority,
-        "TextChangedListener" could've been used to check which field was changed last but )
-        no such requirement was specified)
-        */
-        if(cl!=theCl){
-            getFieldFh().setText(String.valueOf((cl * 1.8) + 32));
-        }else {
-            getFieldCel().setText(String.valueOf((fh-32)/1.8));
-        }
-        theCl=cl;
+        btnControl.buttonCalculateAction();
     }
-
-
+    public void buttonResetAction(View view) {
+        btnControl.buttonResetAction();
+    }
 }
---
+++ app/src/main/java/com/example/temperatureconverter/controller/ButtonController.java
@@ -0,0 +1,59 @@
+package com.example.temperatureconverter.controller;
+
+import android.view.View;
+
+import com.example.temperatureconverter.MainActivity;
+import com.example.temperatureconverter.R;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class ButtonController {
+
+    //  buffer variable that is used to monitor if Celsius is changed
+    private double theCl=0;
+    //Using the variable to access the View
+    private View view;
+
+    public ButtonController(MainActivity theActivity){
+        //Linking the View to the controller from the Main
+        this.view=theActivity.getView();
+    }
+
+    //    Getter for the Celsius field
+    private TextInputEditText getFieldCel()
+    {
+        return (TextInputEditText) view.findViewById(R.id.NumberBoxCel);
+    }
+    //    getter for fahrenheit field
+    private TextInputEditText getFieldFh()
+    {
+        return (TextInputEditText) view.findViewById(R.id.NumberBoxFh);
+    }
+    //    function to reset the values
+    public void buttonResetAction() {
+        getFieldCel().setText("0");
+        getFieldFh().setText("32");
+    }
+    //function to calculate and set the values
+    public void buttonCalculateAction() {
+        //parsing the values from String to double
+        double cl=Double.parseDouble(String.valueOf(getFieldCel().getText()));
+        double fh=Double.parseDouble(String.valueOf(getFieldFh().getText()));;
+        /*
+        the way i'm checking which field is changed is by comparing
+        the buffer variable to the current value of Celsius.
+        -in case Celsius is changed, Fahrenheit is calculated
+        -in case Celsius is not changed, that could mean the FH was changed,
+        therefore Celsius is calculated
+        -in case neither is changed, there would be no change(except maybe making CL from int to double)
+        -in case both are changed Celsius takes priority (it was not specified which one takes priority,
+        "TextChangedListener" could've been used to check which field was changed last but )
+        no such requirement was specified)
+        */
+        if(cl!=theCl){
+            getFieldFh().setText(String.valueOf((cl * 1.8) + 32));
+        }else {
+            getFieldCel().setText(String.valueOf((fh-32)/1.8));
+        }
+        theCl=cl;
+    }
+}
